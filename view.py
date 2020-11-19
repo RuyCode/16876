a0efee2d6c902e4bf7e0f6f42d06768f1d09691f
PixCED <73798574+PixCED@users.noreply.github.com>
2020-11-19 18:30:38 +0300

    Add files via upload

---
+++ view.py
@@ -0,0 +1,7 @@
+from tkinter import PhotoImage
+
+quit_s = PhotoImage(file="Sprites/System/quit.png")
+info_s = PhotoImage(file="Sprites/System/info.png")
+ports_s = PhotoImage(file="Sprites/programs/portscan.png")
+infotxt_s = PhotoImage(file="Sprites/System/infotxt.png")
+sprites = [quit_s, info_s, ports_s, infotxt_s]
